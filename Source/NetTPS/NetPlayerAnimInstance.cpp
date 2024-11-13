ed829fe139bf5efd74f9b776274a6b1b710ba294
lokimve7 <lokimve7@gmail.com>
2024-11-13 11:55:02 +0900

    Initial commit

---
+++ Source/NetTPS/NetPlayerAnimInstance.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NetPlayerAnimInstance.h"
+#include "NetTPSCharacter.h"
+
+void UNetPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 내가 붙어있는 Pawn 가져오자
+	player = Cast<ANetTPSCharacter>(TryGetPawnOwner());
+}
+
+void UNetPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+}
